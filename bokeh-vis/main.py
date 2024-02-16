b55babb6d93bcf52602aee1833631eb628dcfca9
agr17 <aregueiro1700@gmail.com>
2024-02-16 23:46:17 +0100

    Code reorganised for bokeh

--- bokeh-vis/main.py
+++ bokeh-vis/main.py
@@ -1,59 +1,11 @@
-from bokeh.models import ColumnDataSource, HoverTool
-from bokeh.plotting import figure
-
-class ConsumptionsVisualization:
-    def __init__(self):
-        super().__init__()
-        self.source = ColumnDataSource(data=self._get_empty_source())
-
-    def _get_empty_source(self):
-        return {
-            'consumption': [],
-            'time': [],
-        }
-
-    def get_plot(self):
-
-        tooltips = [
-            ("Día", "@formatted_date"),
-            ("Hora", "@formatted_time"),
-            ("Consumo ", "$y KWh"),
-        ]
-
-        p = figure(
-            title="Gráfica de Consumo",
-            x_axis_label='Día',
-            y_axis_label='Consumo (KWh)',
-            sizing_mode="stretch_width",
-            x_axis_type="datetime",
-            tooltips=tooltips
-        )
-
-        p.line(x='time', y='consumption', source=self.source)
-
-        return p
-
-    def update_source(self, new_data):
-        self.source.data = {
-            'consumption': new_data['consumo'],
-            'time': new_data['datetime'],
-            'formatted_date': new_data['datetime'].dt.strftime('%d-%m-%Y'),
-            'formatted_time': new_data['datetime'].dt.strftime('%H:%M') # Add this line
-        }
-
-####################################################################################################
+from consumptions_visualizer import ConsumptionsVisualizer
+from utils.data import load_data
 
 from bokeh.plotting import curdoc
 from bokeh.layouts import column
+
 import pandas as pd
 import sys
-
-def load_data(csv_path):
-    df = pd.read_csv(csv_path)
-    df['fecha'] = df['fecha'].apply(lambda x: pd.Timestamp(x))
-    df['datetime'] = pd.to_datetime(df['datetime'])
-
-    return df
 
 # Get the arguments
 if len(sys.argv) != 2:
@@ -64,7 +16,7 @@ csv_path = sys.argv[1]
 
 df = load_data(csv_path)
 
-consumption_vis = ConsumptionsVisualization()
+consumption_vis = ConsumptionsVisualizer()
 consumption_vis.update_source(df)
 
 plot = consumption_vis.get_plot()
