eaa4d695728935128187855acd64574ccc7b45d1
agr17 <aregueiro1700@gmail.com>
2024-02-17 13:15:57 +0100

    Data group by factors

--- bokeh-vis/general_bars.py
+++ bokeh-vis/general_bars.py
@@ -1,4 +1,4 @@
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, FactorRange
 from bokeh.plotting import figure
 import datetime
 
@@ -6,6 +6,12 @@ class GeneralBars:
     def __init__(self):
         super().__init__()
         self.source = ColumnDataSource(data=self._get_empty_source())
+        self.factors = [
+            ("Q1", "jan"), ("Q1", "feb"), ("Q1", "mar"),
+            ("Q2", "apr"), ("Q2", "may"), ("Q2", "jun"),
+            ("Q3", "jul"), ("Q3", "aug"), ("Q3", "sep"),
+            ("Q4", "oct"), ("Q4", "nov"), ("Q4", "dec"),
+        ]
 
     def _get_empty_source(self):
         return {
@@ -20,10 +26,10 @@ class GeneralBars:
             x_axis_label='Mes',
             y_axis_label='Gasto (€)',
             sizing_mode="stretch_width",
-            x_axis_type="datetime",
+            x_range=FactorRange(*self.factors)
         )
 
-        p.vbar(x='time', top='consumption', width=datetime.timedelta(days=20), source=self.source)
+        p.vbar(x='time', top='consumption', width=0.5, source=self.source)
         p.line(x='time', y='cost', line_width=2, color="red", source=self.source)
 
         return p
@@ -33,5 +39,5 @@ class GeneralBars:
         self.source.data = {
             'consumption': new_data['consumo'],
             'cost': new_data['price'],
-            'time': new_data['month']
+            'time': self.factors,
         }
