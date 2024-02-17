4cfd4b6eb6ab514aaad00c8d815996cbb697562b
Pablo Boo <pabloboocastrofeito@gmail.com>
2024-02-17 12:28:42 +0100

    Integrate top percentage in bokeh

--- bokeh-vis/main.py
+++ bokeh-vis/main.py
@@ -6,6 +6,8 @@ from bokeh.layouts import column
 
 import sys
 
+from top_percentage_consumers import calcular_porcentaje_top
+
 # Get the arguments
 if len(sys.argv) != 2:
     print("Usage: bokeh serve bokeh-vis [--show] --args <csv>")
@@ -23,3 +25,8 @@ plot = consumption_vis.get_plot()
 layout = column(plot, sizing_mode="stretch_width")
 curdoc().add_root(layout)
 
+# Obtener el porcentaje top
+year = "2021"
+porcentaje_top = calcular_porcentaje_top(csv_path, year)
+curdoc().template_variables["porcentaje_top"] = porcentaje_top
+curdoc().template_variables["year"] = year
---
+++ bokeh-vis/top_percentage_consumers.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import os
+
+def calcular_porcentaje_top(csv_path, year):
+    # Directorio donde se encuentran los archivos CSV de los CUPS
+    data_directory = ".\data\cups"
+
+    # Obtener la lista de archivos en el directorio
+    archivos_cups = os.listdir(data_directory)
+    # Contar el número de archivos en el directorio
+    numero_total_cups = len(archivos_cups)
+
+    # Lista para almacenar los consumos totales de los CUPS
+    consumos_totales_cups = []
+
+    # Iterar sobre los archivos de datos de CUPS (electrodatos_cups_id.csv)
+    for cups_id in range(numero_total_cups):
+        # Ruta al archivo CSV correspondiente al CUPS actual
+        file_path = os.path.join(data_directory, f"electrodatos_{cups_id}.csv")
+        
+        # Verificar si el archivo es diferente al proporcionado por el usuario
+        if file_path != csv_path:
+            data = pd.read_csv(file_path)
+            
+            # Filtrar los datos por año
+            data = data[data['datetime'].str.startswith(year)]
+            # Sumar el consumo total del CUPS actual y agregarlo a la lista
+            consumos_totales_cups.append(data['consumo'].sum())
+        else:
+            # Si el fichero está entre los datos no contabilizarlo
+            numero_total_cups=numero_total_cups-1
+
+    # Calcular el consumo total del CUPS del input
+    file_path_input = os.path.join(csv_path)
+    data_input = pd.read_csv(file_path_input)
+    data_input = data_input[data_input['datetime'].str.startswith(year)]
+    consumo_total_input = data_input['consumo'].sum()
+
+    # Cálculo Porcentaje
+    # Contar cuántos consumos totales de los primeros 10 CUPS son menores que el consumo total del décimo CUPS
+    consumos_superados = sum(1 for consumo_total in consumos_totales_cups if consumo_total < consumo_total_input)
+
+    # Calcular el porcentaje de usuarios superados
+    porcentaje_superado = (consumos_superados / numero_total_cups) * 100
+    porcentaje_top = 100 - porcentaje_superado
+
+    # Mostrar el porcentaje de usuarios superados
+    # print(f"Estás en el top {porcentaje_top:.2f}% de los consumidores")
+    return porcentaje_top
