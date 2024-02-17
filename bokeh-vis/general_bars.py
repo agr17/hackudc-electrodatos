f927c036c37ab10e90a7a45947b3fe93a968af28
agr17 <aregueiro1700@gmail.com>
2024-02-17 14:51:41 +0100

    Bar plots complete

--- bokeh-vis/general_bars.py
+++ bokeh-vis/general_bars.py
@@ -1,16 +1,16 @@
-from bokeh.models import ColumnDataSource, FactorRange
+from bokeh.models import ColumnDataSource, FactorRange, HoverTool
 from bokeh.plotting import figure
-import datetime
-
+from bokeh.transform import dodge
+from bokeh.models import LinearAxis
 class GeneralBars:
     def __init__(self):
         super().__init__()
         self.source = ColumnDataSource(data=self._get_empty_source())
         self.factors = [
-            ("Q1", "jan"), ("Q1", "feb"), ("Q1", "mar"),
-            ("Q2", "apr"), ("Q2", "may"), ("Q2", "jun"),
-            ("Q3", "jul"), ("Q3", "aug"), ("Q3", "sep"),
-            ("Q4", "oct"), ("Q4", "nov"), ("Q4", "dec"),
+            ("Q1", "Enero"), ("Q1", "Febrero"), ("Q1", "Marzo"),
+            ("Q2", "Abril"), ("Q2", "Mayo"), ("Q2", "Junio"),
+            ("Q3", "Julio"), ("Q3", "Agosto"), ("Q3", "Septiembre"),
+            ("Q4", "Octubre"), ("Q4", "Noviembre"), ("Q4", "Diciembre"),
         ]
 
     def _get_empty_source(self):
@@ -21,16 +21,31 @@ class GeneralBars:
         }
 
     def get_plot(self):
+
         p = figure(
             title="Gráfica de Gasto",
             x_axis_label='Mes',
-            y_axis_label='Gasto (€)',
             sizing_mode="stretch_width",
-            x_range=FactorRange(*self.factors)
+            x_range=FactorRange(*self.factors),
+            toolbar_location=None,
         )
 
-        p.vbar(x='time', top='consumption', width=0.5, source=self.source)
-        p.line(x='time', y='cost', line_width=2, color="red", source=self.source)
+        vbar_consumption = p.vbar(x=dodge('time', -0.20, range=p.x_range), top='consumption', width=0.3, source=self.source, color="skyblue", legend_label="Consumo")
+        vbar_cost = p.vbar(x=dodge('time', 0.20, range=p.x_range), top='cost', width=0.3, source=self.source, color="chocolate", legend_label="Coste")
+
+        # Twin y axis
+        p.yaxis.axis_label = "Gasto (€)"
+        p.yaxis.axis_label_text_color ="chocolate"
+
+        p.extra_y_ranges = {"consumption": p.y_range}
+        p.add_layout(LinearAxis(y_range_name="consumption", axis_label="Consumo (kWh)", axis_label_text_color="skyblue"), 'right')
+
+        # Toolstips
+        consumption_tool = HoverTool(renderers=[vbar_consumption], tooltips=[('Consumo', '@consumption{0.00} kWh')])
+        cost_tool = HoverTool(renderers=[vbar_cost], tooltips=[('Coste', '@cost{0.00} €')])
+        p.add_tools(consumption_tool, cost_tool)
+
+        print(self.source.data)
 
         return p
 
